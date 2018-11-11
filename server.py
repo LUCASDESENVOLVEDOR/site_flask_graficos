8c7838c3ee4e56040ff5dd4ee1c337c6d479545f
Lucas Desenv <lucas_aragao_fel@hotmail.com>
2018-11-11 17:56:26 -0200

    entrega

--- server.py
+++ server.py
@@ -7,20 +7,7 @@ import seaborn as sb
 from flask import request
 import urllib
 import random
-Dados = pd.read_excel('C:/aula8/PesquisaAuto.xlsx', 'Pesquisa')
-
-
-import random
-
-
-
-
-#Mplt.hist(Dados["Imagem"], bins=5)
-#Mplt.savefig("static\Grafico2.png")
-
-#sb.jointplot(x="Preco", y="Imagem",  data=Dados)
-#Mplt.savefig('site\Grafico.png')
-
+Dados = pd.read_excel('PesquisaAuto.xlsx', 'Pesquisa')
 
 
 
@@ -33,53 +20,94 @@ def Home():
   for x in range(10):
    valor =  random.randint(1,2000)   
 
-
    idgrafico = request.args.get("id", 0, type=int)
+   var1 = request.args.get("var1", 0, type=int)
+   var2 = request.args.get("var2", 0, type=int)
+
 
    if idgrafico == 0:
         return render_template ("index.html")
    else:
         if idgrafico == 1: #BOX-PLOT
-           
-             #criar regra no js para esse grafico apenas 1 coluna.             
-                           
-            sb.boxplot(y="Imagem", data=Dados)         
-            #Mplt.savefig("site\img" + str(idgrafico) + str(valor) +".png") 
+            coluna = nomedacoluna(var1)                            
+            sb.boxplot(y=coluna, data=Dados) 
             Mplt.savefig("site\graficonovo.png")
-            Mplt.clf()            
-            #return render_template ("index.html",Nome="img" +  str(idgrafico) + str(valor) +".png?valor="+str(valor))   
-            return render_template ("index.html",Nome="graficonovo.png?valor="+str(valor))   
-        
-
-        if idgrafico == 2: #HISTOGRAMA COM DENSIDADE
-             #criar regra no js para esse grafico apenas 1 coluna.
-                           
-            sb.distplot(Dados["Imagem"], bins=10, kde=True)           
-            #Mplt.savefig("site\img" + str(idgrafico) + str(valor) +".png")
+            Mplt.clf()
+            return render_template ("index.html",Nome="graficonovo.png?valor="+str(valor))  
+        if idgrafico == 2: #HISTOGRAMA COM DENSIDADE                        
+            coluna = nomedacoluna(var1)   
+            sb.distplot(Dados[coluna], bins=10, kde=True) 
             Mplt.savefig("site\graficonovo.png")
             Mplt.clf()              
-            return render_template ("index.html",Nome="graficonovo.png?valor="+str(valor))   
+            return render_template ("index.html",Nome="graficonovo.png?valor="+str(valor))  
+        if idgrafico == 3: #BARRAS COM MÉDIAS            
+            coluna = nomedacoluna(var1)   
+            coluna2 = nomedacoluna(var2)                
+            sb.barplot(x=coluna, y=coluna2, data=Dados)
+            Mplt.savefig("site\graficonovo.png")
+            Mplt.clf()              
+            return render_template ("index.html",Nome="graficonovo.png?valor="+str(valor))  
+        if idgrafico == 4: #BOX-PLOT CATEGORIZADO
+            coluna = nomedacoluna(var1)   
+            coluna2 = nomedacoluna(var2) 
+            sb.boxplot(x=coluna, y=coluna2, data=Dados) 
+            Mplt.savefig("site\graficonovo.png")
+            Mplt.clf()              
+            return render_template ("index.html",Nome="graficonovo.png?valor="+str(valor))
+        if idgrafico == 5: #Dispersão com densidade
+            coluna = nomedacoluna(var1)   
+            coluna2 = nomedacoluna(var2)           
+            sb.kdeplot(Dados[coluna], Dados[coluna2])          
+            Mplt.savefig("site\graficonovo.png")
+            Mplt.clf()              
+            return render_template ("index.html",Nome="graficonovo.png?valor="+str(valor))  
         
         else:
             return render_template ("index.html",Nome=idgrafico)           
 
 
-@App.route("/histograma", methods=["GET"])
-def Contato():
-
- 
-   idgrafico = request.args.get("id", 0, type=int)    
-   return render_template ("histograma.html",Nome=idgrafico)
-  
 
 
+def nomedacoluna(argument):
+
+    #return "Imagem"
 
 
-@App.route("/lira", methods=["GET"])
-def Contato2():
-  return render_template ("histograma.html")
+    if (argument == 1):
+      return "Respondente"
 
+    if (argument == 2):
+      return "Imagem"
 
+    if (argument == 3):
+      return "Utilitario"
+
+    if (argument == 4):
+      return "Preco"
+
+    if (argument == 5):
+      return "Genero"
+
+    if (argument == 6):
+      return "Idade"
+
+    if (argument == 7):
+      return "Filhos"
+
+    if (argument == 8):
+      return "Renda"
+
+       
+    if (argument == 9):
+      return "Pequeno"
+
+    if (argument == 10):
+      return "Prototipo"
+
+    
+    if (argument == 11):
+      return "EstadoCivil"
+   
 
 
 if __name__ == "__main__":
